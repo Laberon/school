c37c2f639534fa7da1486a3d84277f8d976828f5
Laberon <laberon83@gmail.com>
2022-11-30 14:42:17 +0300

    3

---
+++ src/main/java/ru/hogwarts/school/SchoolApp.java
@@ -0,0 +1,15 @@
+package ru.hogwarts.school;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class SchoolApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApp.class, args);
+	}
+
+}
